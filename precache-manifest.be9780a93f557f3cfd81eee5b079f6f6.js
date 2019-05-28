82e359d0b8105f55b12d5c4ead4c523f6f0781c5
Marcell CISZEK <marcell.ciszek@alten.se>
2019-05-28 12:00:45 +0200

    Updates

---
+++ precache-manifest.be9780a93f557f3cfd81eee5b079f6f6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "964608006046787448275e0183dae69a",
+    "url": "/context_contacts_app/index.html"
+  },
+  {
+    "revision": "100615a3a0d4620fa7a9",
+    "url": "/context_contacts_app/static/js/2.396f07eb.chunk.js"
+  },
+  {
+    "revision": "e500b3c090f5ce97af7a",
+    "url": "/context_contacts_app/static/js/main.d1c02b48.chunk.js"
+  },
+  {
+    "revision": "024bfba64ed5511f014b",
+    "url": "/context_contacts_app/static/js/runtime~main.be35c0c0.js"
+  }
+]);
